0329c58fd3ee26a3253d1b91a5ccb27ac9eec83d
Aerobaydev <medai@aerobay.in>
2025-09-08 12:23:22 +0530

    SEO updates

--- app/updates/page.js
+++ app/updates/page.js
@@ -1,7 +1,12 @@
-"use client"
 import Navbar from "../components/Navbar"
 import Updates from "../components/Updates"
 import Footer from "../components/Footer"
+
+export const metadata = {
+  alternates: {
+    canonical: "https://worldskillchallenge.com/updates",
+  },
+}
 
 export default function UpdatesPage() {
   return (
